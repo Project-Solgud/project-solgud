a4551afcd0e27af78f64344939cffdd031e2b81e
Alan Zhu <alanyzhu@gmail.com>
2018-02-25 00:59:33 +0000

    improve picture quality

--- camera.sh
+++ camera.sh
@@ -3,6 +3,6 @@
 while :
 do
   DATE=$(date +"%Y-%m-%d_%H%M%S")
-  raspistill -n -q 100 -t 100 -o /home/pi/camera/$DATE.jpg
+  raspistill -ex auto -awb auto -n -q 100 -t 100 -o /home/pi/camera/$DATE.jpg
   sleep 30
 done
